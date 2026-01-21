9da5a071f8615f98f769981ccee87e614acdabe8
liton <litonmioa@gmail.com>
2026-01-21 21:45:15 +0600

    variable

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("I like pizza\n");
-        System.out.print("It's really good\n");
-        System.out.println("Buy me pizza");
+        int age = 14;
+        int year = 2026;
+        int quantity = 1;
 
+        double price = 19425.32;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = false;
+        boolean forSale = true;
+        boolean isOnline = true;
+
+        String name = "Bro Code";
+        String food = "pizza";
+        String email = "litonmioa@gmail.com";
+        String car = "Mustang";
+        String color = "red";
+
+        System.out.println("Your choice is a " + color + " " + year + " " + car);
+        System.out.println("The price is: " + currency + price);
+
+        if (forSale) {
+            System.out.println("There is a " + car + " for sale");
+
+        }
+        else {
+            System.out.println("The " + car + " is not for sale");
+        }
     }
 }
