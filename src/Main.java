8554a9eb217012794cf368e901f8914c0622957f
liton <litonmioa@gmail.com>
2026-01-21 20:45:17 +0600

    first

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("I like pizza\n");
+        System.out.print("It's really good\n");
+        System.out.println("Buy me pizza");
+
+    }
+}
