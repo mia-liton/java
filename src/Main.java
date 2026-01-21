3f9d7a5a8a07687bd8ecc840eb067cc5249758aa
liton <litonmioa@gmail.com>
2026-01-21 22:25:44 +0600

    calculate the area

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
 
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
 
-        System.out.print("What is your gpa: ");
-        double gpa = scanner.nextDouble();
+        area = width * height;
 
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
+        System.out.println("The area is: " + area + "cm^2");
 
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old" );
-        System.out.println("You gpa is: " + gpa);
-
-        if (isStudent) {
-            System.out.println("You are enrolled as a student");
-
-        }
-        else {
-            System.out.println("You are NOT enrolled");
-        }
 
         scanner.close();
 
