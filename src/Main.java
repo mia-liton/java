de0f1cb70923aef24f9abe5e1818fa2b8ae613d4
liton <litonmioa@gmail.com>
2026-01-23 14:47:09 +0600

    ternary operator

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        int income = 30000;
 
-        double weight;
-        double newWeight;
-        int choice;
+        double taxRate = (income >= 40000) ? 0.5 : 0.15;
 
-        System.out.println("Weight Conversion Program");
-        System.out.println("1: Convert lbs to kgs");
-        System.out.println("2: Convert khs to lbs");
-
-        System.out.print("Choice an option: ");
-        choice = scanner.nextInt();
-
-        if (choice == 1) {
-            System.out.print("Enter the weight in lbs: ");
-            weight = scanner.nextDouble();
-            newWeight = weight * 0.453592;
-            System.out.printf("The new weight in kgs is: %.2f", newWeight);
-        }
-
-        else if (choice == 2) {
-            System.out.print("Enter the weight in kgs: ");
-            weight = scanner.nextDouble();
-            newWeight = weight * 2.20462;
-            System.out.printf("The new weight in lbs is: %.2f", newWeight);
-
-        }
-        else {
-            System.out.println("That was not a valid choice");
-        }
-
-        scanner.close();
+        System.out.println(taxRate);
 
     }
 }
