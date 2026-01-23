816ce675ec05b5f97a27b3ab683a79f1f1e90b85
liton <litonmioa@gmail.com>
2026-01-23 11:00:18 +0600

    if else statement

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,50 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String item;
-        double price;
-        int quantity;
-        char currency = '$';
-        double total;
+        String name;
+        int age;
+        boolean isStudent;
 
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
-        System.out.print("How many would you like?: ");
-        quantity = scanner.nextInt();
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
 
-        total = price * quantity;
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
 
-        System.out.println("\nYou have bought " + quantity + " " + item +"/s");
-        System.out.println("Your total is " + currency + total);
+        System.out.print("Are you a student (true/false): ");
+        isStudent = scanner.nextBoolean();
+
+        // group 1
+        if (name.isEmpty()) {
+            System.out.println("You didn't enter your name");
+        } else {
+            System.out.println("\nHello " + name + "!");
+        }
+
+        // group 2
+        if (age >= 65) {
+            System.out.println("You are a senior");
+        } else if (age >= 18) {
+            System.out.println("You are an adult");
+
+        } else if (age < 0) {
+            System.out.println("You haven't been born yet");
+
+        } else if (age == 0) {
+            System.out.println("You are a baby");
+
+        } else {
+            System.out.println("You are a child");
+        }
+
+        // group 3
+        if (isStudent) {
+            System.out.println("You are a student");
+        } else {
+            System.out.println("You are NOT a student");
+        }
 
         scanner.close();
+
     }
 }
