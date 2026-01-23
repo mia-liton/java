9cd7e73d37c8b8ceac9902c6858fc46210ca39d0
liton <litonmioa@gmail.com>
2026-01-23 14:09:48 +0600

    substring

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String name = "Bro Code";
+        Scanner scanner = new Scanner(System.in);
 
-//        int length = name.length();
-//        char letter = name.charAt(0);
-//        int index = name.indexOf(" ");
-//        int lastIndex = name.lastIndexOf("o");
+        String email;
+        String username;
+        String domain;
 
-//        name = name.toUpperCase();
-//        name = name.toLowerCase();
+        System.out.print("Enter your email: ");
+        email = scanner.nextLine();
 
-//        if (name.isEmpty()) {
-//            System.out.println("Your name is empty");
-//        }
-//        else {
-//            System.out.println("Hello, " + name);
-//        }
+        if (email.contains("@")) {
 
-        if (name.equalsIgnoreCase("password")) {
-            System.out.println("Your name can't be password");
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+
+            System.out.println(username);
+            System.out.println(domain);
+
         }
         else {
-            System.out.println("Hello, " + name);
+            System.out.println("Emails must contain @");
         }
+
+        scanner.close();
 
     }
 }
