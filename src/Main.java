bdabe34e5b7f7518bdaf201dd0649e3bd4623a39
liton <litonmioa@gmail.com>
2026-01-23 12:37:05 +0600

    math example

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,21 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        double a;
-        double b;
-        double c;
+        double radius;
+        double circumference;
+        double area;
+        double volume;
 
-        System.out.print("Enter the length of side A: ");
-        a = scanner.nextDouble();
+        System.out.print("Enter the radius: ");
+        radius = scanner.nextDouble();
 
-        System.out.print("Enter the length of side B: ");
-        b = scanner.nextDouble();
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
 
-        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-        System.out.println("The hypotenuse (side c) is: " + c + "cm");
+        System.out.printf("The circumference is: %.1fcm\n", circumference);
+        System.out.printf("The area is: %.1fcm^2\n", area);
+        System.out.printf("The volume is: %.1fcm^3\n",  volume);
 
         scanner.close();
 
