dd55e2df9d06bfed496e79f5b146f7c9ac5b7b7b
liton <litonmioa@gmail.com>
2026-01-24 11:48:53 +0600

    calculator propram

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,44 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the day of the week: ");
-        String day = scanner.nextLine();
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+        boolean validOperation = true;
 
-        switch (day) {
-            case "Monday", "Tuesday", "Wednesday", "Thursday" ,"Friday" -> System.out.println("It is a weekday");
-            case "Sunday", "Saturday" -> System.out.println("It is the weekend");
-            default -> System.out.println(day + " is not a day");
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if (num2 == 0) {
+                    System.out.println("Can't divide by zero");
+                    validOperation = false;
+
+                }
+                else {
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator");
+                validOperation = false;
+            }
+        }
+
+        if (validOperation) {
+            System.out.println(result);
         }
 
         scanner.close();
