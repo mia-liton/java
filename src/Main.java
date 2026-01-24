172bb92ea6dbc81d13e22fdaea45ac19e8517b22
liton <litonmioa@gmail.com>
2026-01-24 12:06:48 +0600

    logical operator

--- src/Main.java
+++ src/Main.java
@@ -5,44 +5,19 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        double num1;
-        double num2;
-        char operator;
-        double result = 0;
-        boolean validOperation = true;
+        String username;
 
-        System.out.print("Enter the first number: ");
-        num1 = scanner.nextDouble();
+        System.out.print("Enter your new username: ");
+        username = scanner.nextLine();
 
-        System.out.print("Enter an operator (+, -, *, /, ^): ");
-        operator = scanner.next().charAt(0);
+        if (username.length() < 4 || username.length() > 12) {
+            System.out.println("Username must be between 4 -12 characters");
 
-        System.out.print("Enter the second number: ");
-        num2 = scanner.nextDouble();
+        } else if (username.contains(" ") || username.contains("_")) {
+            System.out.println("Username must not contain spaces or underscores");
 
-        switch (operator) {
-            case '+' -> result = num1 + num2;
-            case '-' -> result = num1 - num2;
-            case '*' -> result = num1 * num2;
-            case '/' -> {
-                if (num2 == 0) {
-                    System.out.println("Can't divide by zero");
-                    validOperation = false;
-
-                }
-                else {
-                    result = num1 / num2;
-                }
-            }
-            case '^' -> result = Math.pow(num1, num2);
-            default -> {
-                System.out.println("Invalid operator");
-                validOperation = false;
-            }
-        }
-
-        if (validOperation) {
-            System.out.println(result);
+        } else {
+            System.out.println("Welcome," + username);
         }
 
         scanner.close();
