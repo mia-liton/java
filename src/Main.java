3cd657b7f52e7c623388eecb68f67a743d0f7187
liton <litonmioa@gmail.com>
2026-02-01 20:06:48 +0600

    2D array project

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        String[][] groceries = {{"apple", "orange", "banana"},
-                               {"potato", "onion", "carrot"},
-                               {"chicken", "pork", "beef", "fish"}};
+        char[][] telephone = {{'1', '2', '3'},
+                              {'4', '5', '6'},
+                              {'7', '8', '9'},
+                              {'*', '0', '#'}};
 
-        groceries[1][0] = "celery";
-
-        for (String[] foods : groceries) {
-            for (String food: foods) {
-                System.out.print(food + " ");
+        for (char[] row : telephone) {
+            for (char number: row) {
+                System.out.print(number + " ");
             }
             System.out.println();
-
         }
 
     }
