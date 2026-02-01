f83d45a058a9da2ff637199919d87d1bdf97a7a0
liton <litonmioa@gmail.com>
2026-02-01 19:14:48 +0600

    entering user data into array

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] fruits = {"apple", "orange", "banana", "coconut"};
-//        fruits[0] = "pineapple";
-//        int numOfFruits = fruits.length;
-//        Arrays.sort(fruits);
-        Arrays.fill(fruits, "pineapple");
+        Scanner scanner = new Scanner(System.in);
+        String[] foods;
+        int size;
 
-        for (String fruit : fruits) {
-            System.out.println(fruit);
+        System.out.print("What # of food do you want?: ");
+        size = scanner.nextInt();
+        scanner.nextLine();
 
+        foods = new String[size];
+
+        for (int i = 0; i < foods.length; i++) {
+            System.out.print("Enter a food: ");
+            foods[i] = scanner.nextLine();
         }
+
+        for (String food : foods) {
+            System.out.println(food);
+        }
+
+        scanner.close();
 
     }
 
