bcfdbbdc0959ecb38fd301b3267c26076bc6bd31
liton <litonmioa@gmail.com>
2026-03-06 20:48:22 +0600

    method overriding

---
+++ src/Fish.java
@@ -0,0 +1,8 @@
+public class Fish extends Animal{
+
+    @Override
+    void move() {
+        System.out.println("The animal is swimming");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Person person = new Person("Harry", "Potter");
-        Student student = new Student("Steve", "Smith", 3.55);
-        Employee employee = new Employee("Joe", "Root", 25000);
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Fish fish = new Fish();
 
-        employee.showName();
-        employee.showSalary();
+        dog.move();
+        cat.move();
+        fish.move();
 
     }
 
