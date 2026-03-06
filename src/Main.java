df097cdd85347c036eb69ead073fa68d63412b92
liton <litonmioa@gmail.com>
2026-03-06 20:09:21 +0600

    inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Friend friend1 = new Friend("Smith");
-        Friend friend2 = new Friend("Roy");
-        Friend friend3 = new Friend("Sarah");
-        Friend friend4 = new Friend("Jenny");
-        Friend friend5 = new Friend("Alex");
-
-       Friend.showFriends();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
 
 
 
---
+++ src/Organism.java
@@ -0,0 +1,10 @@
+public class Organism {
+
+    boolean isAlive;
+
+    Organism () {
+        isAlive = true;
+    }
+
+
+}
