9bb0ace36c6963b6f8c0764337b3025536248f29
liton <litonmioa@gmail.com>
2026-04-10 11:36:38 +0600

    getters, setters

---
+++ src/Car.java
@@ -0,0 +1,43 @@
+public class Car {
+
+    private String model;
+    private String color;
+    private int price;
+
+    Car(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+    String getColor(){
+        return this.color;
+    }
+
+    String getPrice(){
+        return "$" + this.price;
+    }
+
+    void setModel(String model) {
+        this.model = model;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    void  setPrice(int price) {
+       if (price < 0) {
+           System.out.println("Price can't be less than zero");
+       }
+       else {
+           this.price = price;
+       }
+    }
+}
+
+
