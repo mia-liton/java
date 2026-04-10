5092529615a456150b9dbb93f98308e62d37b823
liton <litonmioa@gmail.com>
2026-04-10 11:05:04 +0600

    polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        Rabbit rabbit = new Rabbit();
-        Hawk hawk = new Hawk();
-        Fish fish = new Fish();
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
 
-        fish.flee();
-        fish.hunt();
+       Vehicle[] vehicles = {car, bike, boat};
 
+       for (Vehicle vehicle : vehicles) {
+           vehicle.go();
+       }
 
     }
 
