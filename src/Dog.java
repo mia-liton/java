2cb09636450b0608650aa5c0b2e64992c14a5d51
liton <litonmioa@gmail.com>
2026-04-11 23:13:06 +0600

    countdown timer program

--- src/Dog.java
+++
@@ -1,6 +0,0 @@
-public class Dog {
-
-    void speak(){
-        System.out.println("The dog goes woof");
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,32 @@
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter # of seconds to countdown from: ");
+        int response = scanner.nextInt();
+
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
 
-            int count = 3;
+            int count = response;
             @Override
             public void run() {
-                System.out.println("Hello");
+                System.out.println(count);
                 count--;
 
-                if (count <= 0) {
-                    System.out.println("Task completed");
+                if (count < 0){
+                    System.out.println("Happy new year");
                     timer.cancel();
                 }
-
             }
         };
 
-        timer.schedule(task, 0, 1000);
+        timer.scheduleAtFixedRate(task, 0, 1000);
 
     }
 
