c37a3fa0d823c5f65b3478398e1a3c96d7cb819e
liton <litonmioa@gmail.com>
2026-04-11 22:43:31 +0600

    date, time

--- src/Main.java
+++ src/Main.java
@@ -1,140 +1,21 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
-        String filePath = "words.txt";
-        ArrayList<String> words = new ArrayList<>();
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
-            String line;
-            while ((line = reader.readLine()) != null) {
-                words.add(line.trim());
+        String newDateTime = dateTime.format(formatter);
 
-            }
+        System.out.println(newDateTime);
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Could not find file");
-        }
-        catch (IOException e) {
-            System.out.println("Something went wrong");
 
-        }
 
-        Random random = new Random();
-        String word = words.get(random.nextInt(words.size()));
-
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Character> wordState = new ArrayList<>();
-        int wrongGuesses = 0;
-
-        for (int i = 0; i < word.length(); i++){
-            wordState.add('_');
-
-        }
-
-        System.out.println("Welcome to java hangman");
-
-        while (wrongGuesses < 6) {
-
-            System.out.print(getHangmanArt(wrongGuesses));
-
-            System.out.print("Word: ");
-
-            for (char c : wordState){
-                System.out.print(c + " ");
-
-            }
-
-            System.out.println();
-            System.out.print("Guess a letter: ");
-            char guess = scanner.next().toLowerCase().charAt(0);
-
-            if (word.indexOf(guess) >= 0){
-                System.out.println("Correct guess\n");
-
-                for (int i = 0; i < word.length(); i++) {
-                    if (word.charAt(i) == guess){
-                        wordState.set(i, guess);
-
-                    }
-                }
-
-                if (!wordState.contains('_')) {
-                    System.out.print(getHangmanArt(wrongGuesses));
-                    System.out.println("You win");
-                    System.out.println("The word was: " + word);
-                    break;
-                }
-            }
-            else {
-                wrongGuesses++;
-                System.out.println("Wrong guess\n");
-            }
-
-        }
-
-        if (wrongGuesses >= 6){
-            System.out.print(getHangmanArt(wrongGuesses));
-            System.out.println("Game over");
-            System.out.println("The word was: " + word);
-        }
-
-        scanner.close();
-
-    }
-    static String getHangmanArt(int wrongGuesses) {
-        return switch (wrongGuesses) {
-            case 0 -> """
-                      
-                      
-                      
-                      """;
-            case 1 -> """
-                       0
-                      
-                      
-                      """;
-            case 2 -> """
-                       0
-                       |
-                      
-                      
-                      """;
-            case 3 -> """
-                       0
-                       /|
-                      
-                      
-                      """;
-            case 4 -> """
-                       0
-                       /|\\
-                      
-                      
-                      """;
-            case 5 -> """
-                       0
-                       /|\\
-                       /
-                      
-                      
-                      """;
-            case 6 -> """
-                       0
-                       /|\\
-                       / \\
-                      
-                      
-                      """;
-            default -> "";
-        };
     }
 
 }
