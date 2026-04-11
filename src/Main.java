12e200a3753cc18116748c5fd88f6ed4745554b9
liton <litonmioa@gmail.com>
2026-04-11 12:08:53 +0600

    how to play audio

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,62 @@
-import java.io.BufferedReader;
+import javax.sound.sampled.*;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String filePath = "/home/liton/test.txt";
+        String filePath = "src\\On The Flip - The Grey Room _ Density & Time.wav";
+        File file = new File(filePath);
 
-        try ( BufferedReader reader = new BufferedReader(new FileReader(filePath))){
-            String line;
-            while((line = reader.readLine()) != null) {
-                System.out.println(line);
+        try (Scanner scanner = new Scanner(System.in);
+             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
 
-            }
+           Clip clip = AudioSystem.getClip();
+           clip.open(audioStream);
 
+          String response = "";
+
+          while (response.equals("Q")) {
+              System.out.println("P = Play");
+              System.out.println("S = Stop");
+              System.out.println("R = Reset");
+              System.out.println("Q = Quit");
+              System.out.print("Enter your choice: ");
+
+              response = scanner.next().toUpperCase();
+
+              switch (response) {
+                  case "P" -> clip.start();
+                  case "S" -> clip.stop();
+                  case "R" -> clip.setMicrosecondPosition(0);
+                  case "Q" -> clip.close();
+                  default -> System.out.println("Invalid choice");
+              }
+
+          }
+            System.out.println("No problems detected");
         }
         catch (FileNotFoundException e) {
             System.out.println("Could not locate file");
 
+        }
+        catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file is not supported");
+        }
+        catch (LineUnavailableException e) {
+            System.out.println("Unable to access audio resource");
         }
         catch (IOException e) {
             System.out.println("Something went wrong");
 
         }
 
+        finally {
+            System.out.println("Bye");
+            scanner.close();
+        }
 
     }
 
